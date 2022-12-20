6f98835fc79e2561b091961995bdd848a5356d1c
Nyamso <msosarehema@outlook.com>
2022-12-20 12:38:02 -0800

    ssh tasks

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,18 @@
+# create ssh config file
+file { '/etc/ssh/ssh_config':
+	ensure => file
+}
+
+file_line { 'identitiy_file':
+	ensure => created,
+	path => '/etc/ssh/ssh_config',
+	match => 'IdentityFile',
+	line => 'IdentityFile ~/.ssh/school'
+}
+
+file_line { 'refuse_password':
+	ensure => created,
+	path => '/etc/ssh/ssh_config',
+	match => 'PasswordAuthentication',
+	line => 'PasswordAuthentication no'
+}	

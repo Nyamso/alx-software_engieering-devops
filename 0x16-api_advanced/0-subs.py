d5a45655681552fc5773a4e838728756310cccaf
Nyamso <msosarehema@outlook.com>
2023-05-11 08:31:33 +0000

    making it exe

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,13 +3,13 @@
 import requests
 
 def number_of_subscribers(subreddit):
-"""Return the total number of subscribers"""
-	url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-	headers =  requests.utils.default_headers()
-	headers.update({'User-Agent': 'My User Agent 1.0'})
-
-	r = requests.get(url, headers=headers).json()
-	subscribers = r.get('data', {}).get('subscribers')
-	if not subscribers:
-		return 0
-	return subscribers
+    """Return the total number of subscribers oa given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")

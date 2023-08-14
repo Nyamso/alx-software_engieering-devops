0804fd3149ff9e5e55100ad445bfeafcbd381199
Nyamso <msosarehema@outlook.com>
2023-08-14 10:34:04 +0000

    rework

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,4 @@
-# Increase the ULIMIT of the default file
+# Increase the User limit
 exec { 'fix--for-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
